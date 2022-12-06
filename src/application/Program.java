7bd14c3b023bc6ee7961f85315f30f030525cefb
Bruno Salata <brunoslima@gmail.com>
2022-12-05 21:30:10 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		//Função que vai imprimir o tabuleiro. Para isso, vamos criar a classe User Interface (UI)
-		UI.printBoard(chessMatch.getPieces());
+		while (true) {
+			//Função que vai imprimir o tabuleiro. Para isso, vamos criar a classe User Interface (UI)
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+
 	}
 
 }
