149d9adb6b770850c1c7ee1dd6587240b4c4eaaf
Bruno Salata <brunoslima@gmail.com>
2022-12-06 13:38:44 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
@@ -13,8 +13,25 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-
 	
+	//método abstrato, porque se trata de movimento de peça (genérico). Fosse pião, rei, bispo, não seria abstrato
+	public abstract boolean[][] possibleMoves();
 	
+	//método não abstrato utilizando um método abstrato
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
 	
+	//implementação padrão de método não abstrato, que utiliza um método abstrato
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for(int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
