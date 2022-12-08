0e689f99690c7275e056fc7e274c274211485a50
Bruno Salata <brunoslima@gmail.com>
2022-12-08 15:19:35 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -62,6 +62,10 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		//verificação se a partida está em check
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	
